b4321601693c56c61be17ef53eeff63cb71849e8
haotian jiang <haotianjiang@haotians-MacBook-Pro.local>
2018-02-23 19:30:22 -0500

    69

---
+++ java/69_Sqrt.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x <= 0) return 0;
+        int left = 1; int right = (int)Math.sqrt(Integer.MAX_VALUE);
+        while (left+1 < right) {
+            int mid = left + (right - left)/2;
+            if (mid*mid > x) right = mid;
+            else if (mid*mid < x) left = mid;
+            else return mid;
+        }
+        if (right*right <= x) return right;
+        return left;
+    }
+}
