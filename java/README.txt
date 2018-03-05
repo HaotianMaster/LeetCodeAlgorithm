d15da49a5c0a6c46830837d7a2396c8786a0f180
haotian jiang <haotianjiang@haotians-MacBook-Pro.local>
2018-03-05 12:50:40 -0500

    clean and update

--- java/README.txt
+++
@@ -1,2 +0,0 @@
-56. Merge Intervals (plz do it again later.)
-287. Find the Duplicate Number
--- java/hw2/58_Length_of_Last_Word.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int lengthOfLastWord(String s) {
-        if (s == null || s.length() == 0) return 0;
-        s = s.trim();
-        String[] strs = s.split(" ");
-        int length = strs[strs.length-1].length();
-        return length;
-    }
-}
